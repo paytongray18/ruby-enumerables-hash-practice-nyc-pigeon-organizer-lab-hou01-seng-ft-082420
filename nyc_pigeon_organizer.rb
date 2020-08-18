f13b9574111b7d8e9f754900346f76552a8ebc12
Payton Gray <paytongray18@gmail.com>
2020-08-18 23:33:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  data.each_with_object({}) do |(key,value) final_array|
+  final_results = data.each_with_object({}) do |(key,value),final_array|
     value.each do |inner_key, names|
       names.each do |name|
         if !final_array[name]
